47402206304cb6f1e0710a3468864269dbdf9663
Eriene Galinato <edgalinato@up.edu.ph>
2026-04-02 16:58:19 +0800

    feat(drafts/assignments): make exportable component

---
+++ src/lib/features/drafts/assignments/index.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  import * as Sheet from '$lib/components/ui/sheet';
+  import { Button } from '$lib/components/ui/button';
+
+  import Loader, { type Props } from './loader.svelte';
+
+  const loaderProps: Props = $props();
+</script>
+
+<Sheet.Root>
+  <Sheet.Trigger>
+    {#snippet child({ props })}
+      <Button variant="outline" class='border-primary text-primary' {...props}>
+        See Drafted Students by Method
+      </Button>
+    {/snippet}
+  </Sheet.Trigger>
+  <Sheet.Content side="right" class="min-w-[50%]">
+    <Sheet.Header>
+      <Sheet.Title>Draft Assignments</Sheet.Title>
+    </Sheet.Header>
+    <div class="overflow-auto px-8 pb-40">
+      <Loader {...loaderProps} />
+    </div>
+  </Sheet.Content>
+</Sheet.Root>
