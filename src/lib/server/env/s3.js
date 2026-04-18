9571f1b0bf28278633fc4c8247acdb532bf40b8a
Basti Ortiz <ortiz@bastidood.dev>
2026-04-18 14:15:49 +0800

    fix(env): reinstate `S3_REGION` as required variable

--- src/lib/server/env/s3.js
+++ src/lib/server/env/s3.js
@@ -6,8 +6,9 @@ import { env } from '$env/dynamic/private';
 if (building) {
   // It doesn't make sense to assert environment variables at build-time.
 } else {
+  assert(typeof env.S3_REGION !== 'undefined');
   assert(typeof env.S3_ACCESS_KEY !== 'undefined');
   assert(typeof env.S3_SECRET_KEY !== 'undefined');
 }
 
-export const { S3_ENDPOINT, S3_REGION, S3_ACCESS_KEY = '', S3_SECRET_KEY = '' } = env;
+export const { S3_ENDPOINT, S3_REGION = '', S3_ACCESS_KEY = '', S3_SECRET_KEY = '' } = env;
